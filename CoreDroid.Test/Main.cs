d0604962cb10cd91accf26142a46d35c411820f7
ralph.bariz <ralph.bariz@ralphs-tech.com>
2012-03-03 17:04:16 +0000

    fixes added mono build script

--- CoreDroid.Test/Main.cs
+++ CoreDroid.Test/Main.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Reflection;
+using CoreDroid.Contract;
 
 namespace CoreDroid.Test
 {
@@ -8,17 +9,25 @@ namespace CoreDroid.Test
 	{
 		public static void Main (string[] args)
 		{
-			SocketClient client = new SocketClient (Convert.ToInt32 (Console.ReadLine ()));
+			try {
+				SocketClient client = new SocketClient (Convert.ToInt32 (Console.ReadLine ()));
 			
-			client.LoadMono (File.OpenRead (Path.Combine (Path.GetDirectoryName (Assembly.GetEntryAssembly ().Location), "CoreDroid.Test.Plugin.dll")));
-			TestService service = client.GetService<TestService> ();
-			string[] contents = service.List ("/");
+				client.LoadMono (File.OpenRead (Path.Combine (Path.GetDirectoryName (Assembly.GetEntryAssembly ().Location), "CoreDroid.Test.Plugin.dll")));
+				TestService service = client.GetService<TestService> ();
+				string[] contents = service.List ("/");
 			
-			foreach (string content in contents)
-				Console.WriteLine (content);
+				foreach (string content in contents)
+					Console.WriteLine (content);
 			
-			service.Close ();
-			client.Close ();
+				service.Close ();
+				client.Close ();
+			} catch (ServiceException ex) {
+				Console.WriteLine ("ServiceException");
+				Console.WriteLine ("\tAssemblyName: " + ex.OriginalAssemblyName);
+				Console.WriteLine ("\tTypeName: " + ex.OriginalTypeName);
+				Console.WriteLine ("\tMessage: " + ex.OriginalMessage);
+				Console.WriteLine ("\tStackTrace: " + ex.OriginalStackTrace);
+			}
 		}
 	}
 }
