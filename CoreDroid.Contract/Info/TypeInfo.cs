877a7e687fd4e12f52a09d8f8aa457f71e567e5d
ralph.bariz <ralph.bariz@gmail.com>
2012-03-09 18:53:01 +0000

    fixes

--- CoreDroid.Contract/Info/TypeInfo.cs
+++ CoreDroid.Contract/Info/TypeInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.Serialization;
 namespace CoreDroid.Contract.Message
 {
 	[DataContract]
-	public class TypeMessage
+	public class TypeInfo
 	{
 		[DataMember(Order = 0)]
 		public bool IsNull { get; private set; }
@@ -30,8 +30,11 @@ namespace CoreDroid.Contract.Message
 				return this.assembly; 
 			}
 		}
-
+		
 		[DataMember(Order = 2)]
+		public bool IsArrayType { get; private set; }
+		
+		[DataMember(Order = 3)]
 		public string TypeName { get; private set; }
         
 		private bool typeSearched = false;
@@ -40,7 +43,7 @@ namespace CoreDroid.Contract.Message
 		public Type Type {
 			get {
 				if (!this.typeSearched) {
-					this.type = this.Assembly != null ? this.Assembly.GetTypes ().Where (t => t.FullName == this.TypeName).FirstOrDefault () : null;
+					this.type = this.Assembly != null ? this.Assembly.GetTypes ().Where (t => t.FullName == this.TypeName || (t.FullName.StartsWith (this.TypeName + "`") && t.GetGenericArguments ().Length == this.GenericArguments.Length)).FirstOrDefault () : null;
 					this.typeSearched = true;
 				}
 
@@ -48,23 +51,34 @@ namespace CoreDroid.Contract.Message
 			}
 		}
 		
-		private TypeMessage ()
+		[DataMember(Order = 4)]
+		public TypeInfo[] GenericArguments;
+		
+		private TypeInfo ()
 		{
 		}
 		
-		public TypeMessage (string assemblyName, string typeName)
+		public TypeInfo (string assemblyName, string typeName)
 		{
 			this.IsNull = false;
 			this.AssemblyName = assemblyName;
+			this.IsArrayType = false;
 			this.TypeName = typeName;
+			this.GenericArguments = null;
 		}
 		
-		public TypeMessage (Type type)
+		public TypeInfo (Type type)
 		{
 			if (type != null) {
 				this.IsNull = false;
 				this.AssemblyName = type.Assembly.GetName ().Name;
-				this.TypeName = type.FullName;
+				this.IsArrayType = type.FullName.EndsWith ("[]");
+				this.TypeName = this.IsArrayType ? type.FullName.Remove (type.FullName.Length - 2) : type.FullName;
+				
+				if (type.IsGenericType) {
+					this.GenericArguments = type.GetGenericArguments ().Select (t => new TypeInfo (t)).ToArray ();
+					this.TypeName = this.TypeName.Remove (this.TypeName.IndexOf ('`'));
+				}
 			} else {
 				this.IsNull = true;
 			}
--- CoreDroid.Test.Plugin/DirectoryService.cs
+++ CoreDroid.Test.Plugin/DirectoryService.cs
@@ -16,7 +16,7 @@ namespace DiskDroid.FileSystem
 		[ServiceMember]
 		public FileSystemItemInfo Get (string path, int lifeTime)
 		{
-			if (path.EndsWith ("/"))
+			if (path.Length > 1 && path.EndsWith ("/"))
 				path = path.Remove (path.Length - 1);
 			
 			if (this.cache.ContainsKey (path) && DateTime.UtcNow > this.cache [path].LoadTime.AddSeconds (lifeTime))
@@ -30,13 +30,13 @@ namespace DiskDroid.FileSystem
 		}
 		
 		[ServiceMember]
-		public FileSystemItemInfo[] GetContents (DirectoryItemInfo directory, int lifeTime)
+		public IEnumerable<FileSystemItemInfo> GetContents (DirectoryItemInfo directory, int lifeTime)
 		{
 			List<FileSystemItemInfo> items = new List<FileSystemItemInfo> ();
 			items.AddRange (Directory.GetDirectories (directory.Path).OrderBy (d => d).Select (d => this.Get (d, lifeTime)));
 			items.AddRange (Directory.GetFiles (directory.Path).OrderBy (f => f).Select (f => this.Get (f, lifeTime)));
 			
-			return items.ToArray ();
+			return items.AsEnumerable ();
 		}
 	}
 }
